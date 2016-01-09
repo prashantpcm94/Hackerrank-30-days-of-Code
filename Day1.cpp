f600a2230e157d856129a734fed9745d782ab4d5
Prashant Kumar Singh <prashantpcm94@gmail.com>
2016-01-09 20:23:01 +0530

    Create Day1.cpp

---
+++ Day1.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    cout<<"Primitive : Double"<<endl;
+    cout<<"Primitive : Character"<<endl;
+    cout<<"Primitive : Boolean"<<endl;
+    cout<<"Primitive : Integer"<<endl;
+    cout<<"Referenced : String"<<endl;
+    cout<<"Primitive : Boolean"<<endl;
+    cout<<"Primitive : Double"<<endl;
+    cout<<"Primitive : Character"<<endl;
+    cout<<"Referenced : String"<<endl;
+    return 0;
+}
