61a313ece814227b87956f0f94ab113a7ede55ea
Prashant Kumar Singh <prashantpcm94@gmail.com>
2016-01-27 20:09:27 +0530

    Create Day27.cpp

---
+++ C++/Day27.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"5"<<endl;
+    cout<<"4 3"<<endl;
+    cout<<"-1 0 1 2\n";
+    cout<<"3 1"<<endl;
+    cout<<"-1 0 1\n";
+    cout<<"5 4"<<endl;
+    cout<<"-2 -1 0 1 2\n";
+    cout<<"6 3\n";
+    cout<<"-2 -1 0 1 2 3\n";
+    cout<<"7 6\n";
+    cout<<"-3 -2 -1 0 1 2 3\n";
+}
